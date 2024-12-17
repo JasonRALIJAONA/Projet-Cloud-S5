22cb6831f3a02cf4c8171e0f0a91edf2ad4d56c1
sergianafr <sergiravelo@gmail.com>
2024-12-17 13:00:31 +0300

    Delete schema.sql

--- schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE fournisseur_identite;
-\c fournisseur_identite
-CREATE TABLE users(
-    id serial PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    mail VARCHAR(50) NOT NULL UNIQUE,
-    mdp VARCHAR(32) NOT NULL,
-    date_inscription TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
